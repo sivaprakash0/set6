768b5d8da25671879f776597ad0f9e37020d96d5
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-16 12:19:49 +0530

    Add files via upload

---
+++ powofno.java
@@ -0,0 +1,13 @@
+package set6;
+
+import java.util.Scanner;
+
+public class powofno {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int b=in.nextInt();
+	int c=(int)Math.pow(a, b);
+	System.out.println(c);
+}
+}
