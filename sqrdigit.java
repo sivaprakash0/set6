6aff0928685129d82a4c4844c22a3a1f23c6bd60
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-16 21:39:18 +0530

    Add files via upload

---
+++ sqrdigit.java
@@ -0,0 +1,16 @@
+package set6;
+
+import java.util.Scanner;
+
+public class sqrdigit {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String no=in.next();
+	char c[]=no.toCharArray();
+	int sum=0;
+	for(int i=0;i<no.length();i++){
+		sum=sum+(c[i]-48)*(c[i]-48);
+	}
+	System.out.println(sum);
+}
+}
