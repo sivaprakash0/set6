39d3cd8d7ebba2e435f4417888de45ab866b4d51
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-17 12:15:21 +0530

    Add files via upload

---
+++ minno.java
@@ -0,0 +1,19 @@
+package set3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class minno {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		if(a<=100000 && a>=1){
+		int ar[]=new int[a];
+		for(int i=0;i<a;i++){
+			ar[i]=in.nextInt();
+		}
+		Arrays.sort(ar);
+		System.out.println(ar[0]);
+	}
+  }
+}
