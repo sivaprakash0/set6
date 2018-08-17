87a8eea88aa3733b1d6d09e6dac6d0271488d5b7
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-17 12:11:35 +0530

    Add files via upload

---
+++ maxno.java
@@ -0,0 +1,17 @@
+package set3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class maxno {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	int ar[]=new int[a];
+	for(int i=0;i<a;i++){
+		ar[i]=in.nextInt();
+	}
+	Arrays.sort(ar);
+	System.out.println(ar[a-1]);
+}
+}
