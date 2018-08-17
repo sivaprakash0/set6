5fdacaddb2d97dd52e75f5dfc0f8bb89e89b5707
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-17 12:18:44 +0530

    Add files via upload

---
+++ sortarray.java
@@ -0,0 +1,21 @@
+package set3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sortarray {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		if(a<=1000 && a>=1){
+		int ar[]=new int[a];
+		for(int i=0;i<a;i++){
+			ar[i]=in.nextInt();
+		}
+		Arrays.sort(ar);
+		for(int i=0;i<a;i++){
+		System.out.print(ar[i]+" ");
+		}
+	}
+  }
+}
