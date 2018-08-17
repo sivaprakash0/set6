7928610aea33dfe564b8e1b784f76e22d16d3ff1
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-17 12:36:18 +0530

    Add files via upload

---
+++ printwithindex.java
@@ -0,0 +1,18 @@
+package set3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class printwithindex {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		int ar[]=new int[a];
+		for(int i=0;i<a;i++){
+			ar[i]=in.nextInt();
+		 }
+		for(int i=0;i<a;i++){
+			System.out.println(ar[i]+" "+i);
+		 }	
+	}
+}
