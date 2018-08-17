5a07b8c11d7c9520a92e0d585a655ba78469a9ae
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-17 12:23:12 +0530

    Add files via upload

---
+++ medianno.java
@@ -0,0 +1,19 @@
+package set3;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class medianno {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int a=in.nextInt();
+		if(a<=1000 && a>=1){
+		int ar[]=new int[a];
+		for(int i=0;i<a;i++){
+			ar[i]=in.nextInt();
+		}
+		Arrays.sort(ar);	
+		System.out.print(ar[a/2]);
+	}
+  }
+}
