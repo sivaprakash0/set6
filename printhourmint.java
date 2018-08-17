e0d90caf2d8111ff6e06f636a0b931d95159d9d1
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-17 13:40:04 +0530

    Add files via upload

---
+++ printhourmint.java
@@ -0,0 +1,17 @@
+package set3;
+
+import java.util.Scanner;
+
+public class printhourmint {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int a=in.nextInt();
+	if(a>60){
+		int b=a/60;
+		System.out.println((a/60)+" "+(a%60));
+	}
+	else{
+		System.out.println("0 "+a);
+	}
+}
+}
